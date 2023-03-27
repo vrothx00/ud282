0abe56a35cf32be21521b02b096a4c3feee859d6
vrothx00 <vrothx00@gmail.com>
2023-03-27 19:37:52 +0200

    Solve problems set 1

---
+++ problem-set-1/assign.java
@@ -0,0 +1,29 @@
+//Question 1 - D
+String name = 'Adam';
+
+//Question 2 - B
+double register = 19.5;
+
+//Question 3 -
+int bankBalance = 500;
+bankBalance += 250;
+bankBalance -= 100;
+System.out.println(bankBalance);
+
+//Question 4 - B 4.0
+
+//Question 5
+int day = 25;
+String month = "August";
+
+//Question 6
+String firstName = "Ali";
+String lastName = "Stark";
+String fullName = firstName + " " + lastName;
+System.out.println("Hello, my name is " + fullName + ".");
+System.out.println("There are " + fullName.length + " letters in my name.");
+
+//Question 7
+double fahrenheit = 20;
+double celsius = (fahrenheit - 32) * 5 / 9;
+System.out.println(celsius);
