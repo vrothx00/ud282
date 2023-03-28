84ddd76b6c56e7c282a74daeef9bb2be78b71a57
vrothx00 <vrothx00@gmail.com>
2023-03-28 14:22:52 +0200

    Init project tictactoe

---
+++ problem-set-5/HelloWorld.java
@@ -0,0 +1,10 @@
+// Java code for printing Hello World
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	//write code you want to execute here
+	System.out.println("Hello java!");
+	}
+
+}
